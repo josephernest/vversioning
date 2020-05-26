79e3468eb013b787412540a6aefcf075e763a4dc
josephernest <nouvellecollection@gmail.com>
2020-05-26 21:05:11 +0200

    Update vversioning.py

--- vversioning.py
+++ vversioning.py
@@ -6,16 +6,12 @@ vversioning
 * detects the lines inside ==CHANGELOG== delimiters in files with extensions listed below (e.g. py, txt, js, c, cpp), or in such files inside ZIP archives
 * lists the differences among these blocks in successive vetrsions
 
-HOMEPAGE
-https://github.com/josephernest/vversioning
-
 USAGE
 python vversioning.py           do it in current folder
 python vversioning.py test/     do it in folder test/
 
 ==CHANGELOG==
-* open encoding
-* new project name
+* ordered display of changelog lines
 ==CHANGELOG==
 """
 
@@ -23,21 +19,26 @@ FILEEXT = ['.txt', '.py', '.js', '.c', '.cpp']
 ARCHIVEEXT = ['.zip']
 import glob, zipfile, os, collections, re, sys
 path = '.' if len(sys.argv) < 2 else sys.argv[1]
-changes = collections.defaultdict(set)
+changes = collections.defaultdict(list)
+def addlines(f, content):
+    for m in re.findall(r"^==CHANGELOG==(.*)^==CHANGELOG==", content, re.MULTILINE | re.DOTALL):
+        for l in m.splitlines():
+            if l != '' and l not in changes[f]:
+                changes[f] += [l]
 for f in sorted(glob.glob(os.path.join(path, '*.*'))):
     if os.path.splitext(f)[1] in ARCHIVEEXT:
         z = zipfile.ZipFile(f, 'r')
         for g in z.namelist():
             if os.path.splitext(g)[1] in FILEEXT:
-                changes[f].update([l for m in re.findall(r"^==CHANGELOG==(.*)^==CHANGELOG==", z.open(g).read().decode(), re.MULTILINE | re.DOTALL) for l in m.splitlines() if l != ''])
+                addlines(f, z.open(g).read().decode())
     elif os.path.splitext(f)[1] in FILEEXT:
         with open(f, 'r', encoding='utf8') as g:
-            changes[f].update([l for m in re.findall(r"^==CHANGELOG==(.*)^==CHANGELOG==", g.read(), re.MULTILINE | re.DOTALL) for l in m.splitlines() if l != ''])
-previous = set()
+                addlines(f, g.read())
+previous = []
 for f in sorted(changes.keys()):
     if changes[f] == set():
         continue
     print(f)
-    print('\n'.join(changes[f] - previous))
+    print('\n'.join([l for l in changes[f] if l not in previous]))
     print("")
     previous = changes[f]
