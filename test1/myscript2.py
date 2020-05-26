164457c6ce956bfe7c31ada35ab3172a76de4b45
josephernest <nouvellecollection@gmail.com>
2020-05-26 13:05:44 +0200

    Create myscript2.py

---
+++ test1/myscript2.py
@@ -0,0 +1,11 @@
+"""
+==CHANGELOG==
+* todo: support UTF8
+* new GUI
+==CHANGELOG==
+"""
+
+sqdgfhsqgfksqfkjgsqfkqsgdkfsqkgfqsdf
+sqgjdfjsqdhfqgskdgfkqgsdjfsqdfggdsqjf
+sqdgfhsqgfksqfkjgsqfkqsgdkfsqkgfqsdf
+sqgjdfjsqdhfqgskdgfkqgsdjfsqdfggdsqjf
