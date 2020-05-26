cfcaad7b700a116d955b7b70505b77074e93e49f
josephernest <nouvellecollection@gmail.com>
2020-05-26 13:05:58 +0200

    Create myscript3.py

---
+++ test1/myscript3.py
@@ -0,0 +1,11 @@
+"""
+==CHANGELOG==
+* todo: support UTF8
+* code refactoring
+==CHANGELOG==
+"""
+
+sqdgfhsqgfksqfkjgsqfkqsgdkfsqkgfqsdf
+sqgjdfjsqdhfqgskdgfkqgsdjfsqdfggdsqjf
+sqdgfhsqgfksqfkjgsqfkqsgdkfsqkgfqsdf
+sqgjdfjsqdhfqgskdgfkqgsdjfsqdfggdsqjf
